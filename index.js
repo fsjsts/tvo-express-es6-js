fcfd2ba95e07fc2c6c235a9ee4c585c57649be7e
aiXpertLab <aiXpertLab@gmail.com>
2024-06-28 02:36:26 -0400

    v1.package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import apiRoutes from "./routes/api";
+import apiRoutes from "./routes/api.js";
 import cors from 'cors';
 
 const app = express();
